5325c8f871e74dfea7f83c45956163556ab2cc5d
Nick Ganter <nickganter91@gmail.com>
2015-05-06 20:55:50 -0500

    adding item list view

---
+++ app/src/main/java/summoner/plus/Build.java
@@ -0,0 +1,15 @@
+package summoner.plus;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Nick on 5/6/2015.
+ */
+public class Build
+{
+    public int BuildID;
+    public int ChampionID;
+    public int UserID;
+    public String BuildName;
+    public ArrayList<Item> Items;
+}
---
+++ app/src/main/java/summoner/plus/Item.java
@@ -0,0 +1,11 @@
+package summoner.plus;
+
+/**
+ * Created by Nick on 5/6/2015.
+ */
+public class Item
+{
+    public int Id;
+    public String Description;
+    public String Name;
+}
